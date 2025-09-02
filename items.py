0ee2522e84ab09c071323beddfc311c6c1759ca6
taj786-coder <117329890+taj786-coder@users.noreply.github.com>
2025-09-02 14:52:48 +0530

    Create items.py

---
+++ items.py
@@ -0,0 +1,8 @@
+# item data (code, name, price, stock)
+prices = {
+    "A101": ("apple", 50, 10),
+    "B202": ("banana", 20, 15),
+    "M303": ("milk", 40, 20),
+    "B404": ("bread", 30, 25),
+    "S505": ("shoes", 1500, 5)
+}
